42f4da1d1caf60e38f7765854eb009ef0fed506e
Rodolfo Gutierrez <rag.ortega@gmail.com>
2021-06-05 19:55:48 -0600

    Juego de naves version 1.0.0

---
+++ cmake-build-debug/CMakeFiles/ship.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ship.dir/main.cpp.obj"
+  "libship.dll.a"
+  "ship.exe"
+  "ship.exe.manifest"
+  "ship.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ship.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
